7504d1fb67e10f5282f0e7f5db674cc5fc18e754
Francesco <francesco.mazzini432@gmail.com>
2023-06-08 07:18:06 +0200

    Added some docs to the SearchBar components
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/fragments/SearchBar.jsx
+++ src/fragments/SearchBar.jsx
@@ -3,26 +3,34 @@ import { Grid } from "@mui/material";
 import {Typography} from "@mui/material";
 import React, { useState, useEffect } from 'react';
 
-const SearchBar = ({query, setQuery}) => {
-    return (
-        <Grid container justifyContent="center" >
-            <Grid item container justifyContent="center" xs={12} style={{margin: "10 0 0 5"}}>
-                <Grid item xs={6}>
-                    <SearchField  query = {query} setQuery =  {setQuery} />
-                </Grid>
-            </Grid>
-            <Grid item container justifyContent="center" xs={12} style={{margin: "5 0 0 5"}}>
-                <Grid item xs={6}>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        results shown for: {query}
-                    </Typography>
-                </Grid>
-            </Grid>
-        </Grid>
-    );
-}
+// const SearchBar = ({query, setQuery}) => {
+//     return (
+//         <Grid container justifyContent="center" >
+//             <Grid item container justifyContent="center" xs={12} style={{margin: "10 0 0 5"}}>
+//                 <Grid item xs={6}>
+//                     <SearchField  query = {query} setQuery =  {setQuery} />
+//                 </Grid>
+//             </Grid>
+//             <Grid item container justifyContent="center" xs={12} style={{margin: "5 0 0 5"}}>
+//                 <Grid item xs={6}>
+//                     <Typography sx={{ mb: 1.5 }} color="text.secondary">
+//                         results shown for: {query}
+//                     </Typography>
+//                 </Grid>
+//             </Grid>
+//         </Grid>
+//     );
+// }
 
 
+/**
+ * Component representing the search bar.
+ * React has a built-in XSS sanification so we call unsafe those components in which we use special React functions to avoid its built-in sanification
+ * and to show that even being unsafe, the sanification is done in backend, as we did in the lab
+ * 
+ * @param query query inserted by the user
+ * @param setQuery function to update the current query set by the user
+ */
 const UnsafeSearchBar = ({query, setQuery}) => {
 
     return(
@@ -42,4 +50,4 @@ const UnsafeSearchBar = ({query, setQuery}) => {
 
 }
 
-export {SearchBar, UnsafeSearchBar};
+export {/*SearchBar, */UnsafeSearchBar};
