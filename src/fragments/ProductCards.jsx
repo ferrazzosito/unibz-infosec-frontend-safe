5dab0876110eadf7ad42a1c6925e064ac257a8a2
Francesco <francesco.mazzini432@gmail.com>
2023-06-08 19:21:53 +0200

    Fix
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/fragments/ProductCards.jsx
+++ src/fragments/ProductCards.jsx
@@ -68,7 +68,7 @@ const UnsafeContentProductCard = ({type, name, price, vendorName, vendorId, desc
  */
 const BasicProductCard = ({type, name, price, vendorId, description}) => {
     
-    const {findUser} = useContext(authContext);    
+    const {user, findUser} = useContext(authContext);    
     
     const [vendor, setVendor] = React.useState({});
     
@@ -89,7 +89,7 @@ const BasicProductCard = ({type, name, price, vendorId, description}) => {
                 vendorName={vendor.email}  
                 vendorId={vendorId} 
                 description={description } 
-                isVendor={vendor.role === "VENDOR"}/>
+                isVendor={user.payload.role === "vendor"}/>
         </Card>
     )
 
